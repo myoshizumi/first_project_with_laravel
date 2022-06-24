af79912f016e4e1931be59b45b9d7fdc9b3e3cdf
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-06-24 16:23:50 +0900

    Add login and logout

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +27,16 @@ Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
+Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
 
 
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
+
+Route::get('/register', [UserController::class, 'create']);
+
+Route::post('/users', [UserController::class, 'store']);
+
+Route::post('/logout', [UserController::class, 'logout']);
