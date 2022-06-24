a2e2ed1c07e389b540ae51361ed514eb0f922ab8
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-06-24 09:26:30 +0900

    Add search function

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,6 +1,4 @@
-@extends('layout')
-
-@section('content')
+<x-layout>
     <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
     </a>
     <div class="mx-4">
@@ -39,4 +37,4 @@
             </div>
         </x-card>
     </div>
-@endsection
+</x-layout>
